12ac14132cf3170832a00e8802edb4107c289796
Benji Smith <smith.benj@husky.neu.edu>
2014-10-23 00:15:51 -0400

    Wrapped crt checks in a check for windows.  The project still might not be cross-platform (needs to be checked).

--- src/main.cpp
+++ src/main.cpp
@@ -17,11 +17,14 @@ using std::cout; using std::endl;
 int main(int argc, char** argv){
 
 #ifdef TESTING
+#if defined(_WIN32) || defined(_WIN64)
+	//Windows memory leak checking
 	_CrtSetReportMode( _CRT_WARN, _CRTDBG_MODE_FILE );
 	_CrtSetReportFile( _CRT_WARN, _CRTDBG_FILE_STDOUT );
 	_CrtSetReportMode( _CRT_ERROR, _CRTDBG_MODE_FILE );
 	_CrtSetReportFile( _CRT_ERROR, _CRTDBG_FILE_STDOUT );
 	_CrtSetDbgFlag ( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
+#endif
 
 	int retVal = RunAllTests();
 
