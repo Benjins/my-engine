2e0dafb8bd994e82c403ba4be7d38080b6f613ee
Benji Smith <smith.benj@husky.neu.edu>
2015-06-05 23:00:51 -0400

    BUMP....sorta.

--- shader.fs
+++ shader.fs
@@ -23,17 +23,19 @@ void main()
 	vec4 texCol  = texture2D(_mainTex, uv_coord.st);
 	vec4 bumpCol = texture2D(_bumpMap, uv_coord.st);
 	texCol.a = 1.0;
-	vec4 color = mix(mix(texCol, bumpCol, 0.5), _color, 0.5);
+	vec4 color = mix(texCol, _color, 0.5);
+	
+	vec3 usedNormal = bumpCol.xyz;//normal * bumpCol.z + tangent * bumpCol.y + cross(normal, tangent) * bumpCol.x;
 	
 	float lighting = 0.05;
 	for(int i = 0; i < numLights; i++){
 		if(lightIsDirectional[i] != 0){
-			lighting += (dot(normal, lightVectors[i]) + 1)/2;
+			lighting += (dot(usedNormal, lightVectors[i]) + 1)/2;
 		}
 		else{
 			vec3 difference = lightVectors[i] - pos;
 			float distance = length(difference);
-			float normalDot = dot(normal, difference/distance);
+			float normalDot = dot(usedNormal, difference/distance);
 			float clampedDist = min(distance, 5);
 			float thisLighting = max(0, (normalDot+1)/2 * (5 - clampedDist));
 			lighting += thisLighting;
