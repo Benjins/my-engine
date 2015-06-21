ee2263a8766d09d6eb2e94ee95d8edb5679d1395
Benji Smith <smith.benj@husky.neu.edu>
2015-06-20 21:29:14 -0400

    Fixed RotateTowards to take the shortest path.

--- src/3DUtilities.cpp
+++ src/3DUtilities.cpp
@@ -1,13 +1,26 @@
 #include "../header/int/3DUtilities.h"
+#include <stdio.h>
 
 Quaternion RotateTowards(const Quaternion& from, const Quaternion& to, float maxDegreesDelta){
+	
 	maxDegreesDelta *= (1.0f/60);
 
 	float distance = (from - to).Magnitude();
+	float otherDistance = (from + to).Magnitude();
 	if(distance < maxDegreesDelta){
 		return to;
 	}
 
-	float weight = maxDegreesDelta/distance;
-	return from * (1 - weight) + to * weight;
+	if(otherDistance < maxDegreesDelta){
+		return to;
+	}
+
+	if(otherDistance < distance){
+		float weight = maxDegreesDelta/otherDistance;
+		return from * (1 - weight) + (to * -weight);
+	}
+	else{
+		float weight = maxDegreesDelta/distance;
+		return from * (1 - weight) + to * weight;
+	}
 }
