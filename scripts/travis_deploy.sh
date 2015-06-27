faf90fcc9e65bbbc4832905754b870423b80b2ec
Benji Smith <smith.benj@husky.neu.edu>
2015-06-27 19:05:47 -0400

    Trying to fix travis g++.

--- scripts/travis_deploy.sh
+++ scripts/travis_deploy.sh
@@ -4,3 +4,4 @@ cd build
 zip my-engine_$CXX.zip my-engine my-engine-editor
 ssh-keygen -R login.ccs.neu.edu
 sshpass -e scp -o 'StrictHostKeyChecking=no' my-engine_$CXX.zip $FTP_USER@login.ccs.neu.edu:~/.www/personal-projects/deployment/my-engine/
+sshpass -e scp -o 'StrictHostKeyChecking=no' -r docs $FTP_USER@login.ccs.neu.edu:~/.www/personal-projects/deployment/my-engine/
