70cb896d9d7d7d541ae87de762cea55700bd7cdd
Benji Smith <smith.benj@husky.neu.edu>
2015-06-27 20:24:18 -0400

    Update remote location of docs.

--- scripts/travis_deploy.sh
+++ scripts/travis_deploy.sh
@@ -3,8 +3,7 @@
 cd build
 zip my-engine_$CXX.zip my-engine my-engine-editor
 ssh-keygen -R login.ccs.neu.edu
-sshpass -e scp -o 'StrictHostKeyChecking=no' my-engine_$CXX.zip $FTP_USER@login.ccs.neu.edu:~/.www/personal-projects/deployment/my-engine/
+sshpass -e scp -o 'StrictHostKeyChecking=no' my-engine_$CXX.zip $FTP_USER@login.ccs.neu.edu:~/.www/personal-projects/my-engine/files/
 
 cd ..
-echo $CXX
-if [ "$CXX" == "g++" ]; then sshpass -e scp -o 'StrictHostKeyChecking=no' -r docs $FTP_USER@login.ccs.neu.edu:~/.www/personal-projects/deployment/my-engine/; fi
+if [ "$CXX" == "g++" ]; then sshpass -e scp -o 'StrictHostKeyChecking=no' -r docs $FTP_USER@login.ccs.neu.edu:~/.www/personal-projects/my-engine/; fi
