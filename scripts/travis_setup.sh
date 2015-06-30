3d4ee0f9e6427559a63842232c9266bc9281262c
Benji Smith <smith.benj@husky.neu.edu>
2015-06-29 21:08:17 -0400

    Mac travis work.

--- scripts/travis_setup.sh
+++ scripts/travis_setup.sh
@@ -10,8 +10,6 @@ sudo apt-get install freeglut3-dev glew1.5-dev libfreetype6-dev libopenal-dev ss
 if [ "$1" == "g++" ]; then sudo apt-get install doxygen; fi
 sudo apt-get update -qq
 
-if [ "$2" == "osx" ]; then sudo brew install openal-soft; fi
-
 #g++4.8.1
 if [ "$1" == "g++" ]; then sudo apt-get install -qq gcc-4.8 g++-4.8; fi
 if [ "$1" == "g++" ]; then sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90; fi
